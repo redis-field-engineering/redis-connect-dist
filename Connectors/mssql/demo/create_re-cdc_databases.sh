936961cb26a15d9db9c375c8e5c8a0df77faee57
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2020-09-22 10:38:18 -0400

    updated the script with 2 db's
    Updated the RE DB creation script to only create 2 db's and added RedisSearch 2.0 on the target db.

--- Connectors/mssql/demo/create_re-cdc_databases.sh
+++ Connectors/mssql/demo/create_re-cdc_databases.sh
@@ -11,25 +11,32 @@ sudo docker run -d --cap-add sys_resource -h re-node3 --name re-node3 -p 38443:8
 echo "Waiting for the servers to start..."
 sleep 60
 echo "Creating Redis Enterprise cluster and joining nodes..."
-sudo docker exec -it --privileged re-node1 "/opt/redislabs/bin/rladmin" cluster create name cluster1.local username demo@redislabs.com password redislabs
+sudo docker exec -it --privileged re-node1 "/opt/redislabs/bin/rladmin" cluster create name re-cluster.local username demo@redislabs.com password redislabs
 sudo docker exec -it --privileged re-node2 "/opt/redislabs/bin/rladmin" cluster join nodes $(sudo docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' re-node1) username demo@redislabs.com password redislabs
 sudo docker exec -it --privileged re-node3 "/opt/redislabs/bin/rladmin" cluster join nodes $(sudo docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' re-node1) username demo@redislabs.com password redislabs
 # Test the cluster 
 sudo docker exec -it re-node1 bash -c "/opt/redislabs/bin/rladmin status"
-# Create a demo database
-echo "Creating demo-db database..."
+# Create demo databases
+echo "Creating databases..."
 rm create_demodb.sh
 tee -a create_demodb.sh <<EOF
-curl -v -k -u demo@redislabs.com:redislabs -X POST https://localhost:9443/v1/bdbs -H Content-type:application/json -d '{ "name": "srcConnection", "port": 12000, "memory_size": 100000000, "type" : "redis", "replication": false}'
-curl -v -k -u demo@redislabs.com:redislabs -X POST https://localhost:9443/v1/bdbs -H Content-type:application/json -d '{ "name": "jobConfigConnection", "port": 12001, "memory_size": 100000000, "type" : "redis", "replication": false}'
-curl -k -L -u demo@redislabs.com:redislabs --location-trusted -X POST https://localhost:9443/v1/bdbs -H "Content-type:application/json" -d '{"name": "metricsConnection", "type":"redis", "memory_size":100000000, "port":12002, "module_list": [{"module_args": "", "module_id": "f3b681f2c740cf9af3bffd5eef302166", "module_name": "timeseries", "semantic_version": "1.2.7"}]}'
+curl -v -k -L -u demo@redislabs.com:redislabs --location-trusted -H Content-type:application/json -d '{ "name": "RedisCDC-Target-db", "port": 12000, "memory_size": 100000000, "type" : "redis", "replication": false, "module_list": [{"module_args": "PARTITIONS AUTO", "module_id": "f181a538611833224950c3d157bd89f9", "module_name": "search", "semantic_version": "2.2.0"}]}' https://localhost:9443/v1/bdbs
+curl -v -k -L -u demo@redislabs.com:redislabs --location-trusted -H "Content-type:application/json" -d '{"name": "RedisCDC-JobConfig-Metrics-db", "type":"redis", "replication": false, "memory_size":100000000, "port":12001, "module_list": [{"module_args": "", "module_id": "f3b681f2c740cf9af3bffd5eef302166", "module_name": "timeseries", "semantic_version": "1.2.7"}]}' https://localhost:9443/v1/bdbs
 EOF
 sudo docker cp create_demodb.sh re-node1:/opt/create_demodb.sh
 sudo docker exec --user root -it re-node1 bash -c "chmod 777 /opt/create_demodb.sh"
 sudo docker exec --user root -it re-node1 bash -c "/opt/create_demodb.sh"
 #sudo docker exec -it re-node1 bash -c "redis-cli -h redis-12000.cluster1.local -p 12000 PING"
-sudo docker exec -it re-node1 bash -c "rladmin status"
-sudo docker port re-node1 | grep 12000
-sudo docker port re-node2 | grep 12001
-sudo docker port re-node3 | grep 12002
+#sleep 60
+# Create RedisSearch index with matching schema as the emp hash to be able to search/aggregate etc.
+sudo docker exec -it re-node1 bash -c "/opt/redislabs/bin/redis-cli -p 12000 ft.create emp on hash prefix 1 emp: schema EmpNum numeric sortable FName text sortable LName text Job tag sortable Manager numeric HireDate text Salary numeric Commission numeric Department numeric"
+
+echo "node1:"
+sudo docker port re-node1 | egrep "12000|12001"
+echo "node2:"
+sudo docker port re-node2 | egrep "12000|12001" 
+echo "node3:"
+sudo docker port re-node3 | egrep "12000|12001" 
+sudo docker exec -it re-node1 bash -c "rladmin status nodes && rladmin status databases && rladmin status shards"
 echo "Now open the browser and access Redis Enterprise Admin UI at https://127.0.0.1:18443 with username=demo@redislabs.com and password=redislabs."
+echo "To connect using RedisInsight or redis-cli, please use the exposed port from the node where master shard for the database resides."
