f40858adae38dc64799c85ffa45650452ea19ca2
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2021-01-13 09:59:55 -0500

    updated sql

--- Connectors/mssql/demo/mssql_cdc.sql
+++ Connectors/mssql/demo/mssql_cdc.sql
@@ -41,12 +41,6 @@ EXEC sys.sp_cdc_enable_table @source_schema = 'dbo'
        , @capture_instance = 'cdcauditing_emp'
 
 -- Query and check the CDC setup
-SELECT s.name AS Schema_Name, tb.name AS Table_Name
-, tb.object_id, tb.type, tb.type_desc, tb.is_tracked_by_cdc
-FROM sys.tables tb
-INNER JOIN sys.schemas s on s.schema_id = tb.schema_id
-WHERE tb.is_tracked_by_cdc = 1
-
--- Or use this Stored proc to return the change data capture configuration for each table enabled for change data capture in the current database.
+-- Use this Stored proc to return the change data capture configuration for each table enabled for change data capture in the current database.
 USE RedisLabsCDC;
 EXEC sys.sp_cdc_help_change_data_capture;
