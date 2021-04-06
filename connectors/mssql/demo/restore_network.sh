25bd2e18b7481aa246a08943ecc8db85a018e5cc
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2021-04-05 22:09:01 -0400

    sudo

--- connectors/mssql/demo/restore_network.sh
+++ connectors/mssql/demo/restore_network.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker network connect network2 rp1
-docker network connect network3 rp1
-docker network connect network1 rp2
-docker network connect network3 rp2
-docker network connect network1 rp3
-docker network connect network2 rp3
+sudo docker network connect network2 rp1
+sudo docker network connect network3 rp1
+sudo docker network connect network1 rp2
+sudo docker network connect network3 rp2
+sudo docker network connect network1 rp3
+sudo docker network connect network2 rp3
