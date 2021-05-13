b79e9d7a1a31f315a7a546397765a87ee8cb5b94
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2021-05-13 14:12:19 -0400

    postgres sql

---
+++ connectors/postgres/demo/postgres_cdc.sql
@@ -0,0 +1,16 @@
+select version();
+
+CREATE TABLE IF NOT EXISTS emp (
+    empno int NOT NULL,
+    fname varchar(50),
+    lname varchar(50),
+    job varchar(50),
+    mgr int,
+    hiredate date,
+    sal decimal(13, 4),
+    comm decimal(13, 4),
+    dept int,
+    PRIMARY KEY (empno)
+    );
+
+\d emp;
