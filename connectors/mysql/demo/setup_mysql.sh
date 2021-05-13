885067d986ce144756874cc24e2b7e4d8ea0c96d
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2021-05-13 16:30:53 -0400

    updated script

--- connectors/mysql/demo/setup_mysql.sh
+++ connectors/mysql/demo/setup_mysql.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# delete the existing mysql:latest container if it exist
-sudo docker kill mysql-latest-$(hostname);sudo docker rm mysql-latest-$(hostname);
+version="$1"
+[[ -z "$version" ]] && { echo "Error: Missing docker version tag e.g. latest, 5.7.33"; exit 1; }
 
-echo "Creating mysql-latest-$(hostname) docker container."
-sudo docker run --name mysql-latest-$(hostname) -v $(pwd)/mysql:/etc/mysql/conf.d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=Redis@123 -d mysql:latest
+# delete the existing mysql:latest container if it exist
+sudo docker kill mysql-$version-$(hostname);sudo docker rm mysql-$version-$(hostname);
+
+echo "Creating mysql-$version-$(hostname) docker container."
+sudo docker run --name mysql-$version-$(hostname) -v $(pwd)/mysql:/etc/mysql/conf.d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=Redis@123 -d mysql:$version
 
 sleep 30s
 
 echo "Creating RedisLabsCDC database and emp table."
 #run the setup script to create the DB and the table in the DB
-sudo docker cp mysql_cdc.sql mysql-latest-$(hostname):mysql_cdc.sql
-sudo docker exec -it mysql-latest-$(hostname) bash -c 'mysql -h"localhost" -P"3306" -uroot -p"$MYSQL_ROOT_PASSWORD" < mysql_cdc.sql'
+sudo docker cp mysql_cdc.sql mysql-$version-$(hostname):mysql_cdc.sql
+sudo docker exec -it mysql-$version-$(hostname) bash -c 'mysql -h"localhost" -P"3306" -uroot -p"$MYSQL_ROOT_PASSWORD" < mysql_cdc.sql'
 echo ""
