aa43f86038b6da255327db0ef395c05dc100d171
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2021-05-14 16:40:35 -0400

    updated with cdc

--- connectors/postgres/demo/setup_postgres.sh
+++ connectors/postgres/demo/setup_postgres.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# delete the existing postgres:latest container if it exist
-sudo docker kill postgres-latest-$(hostname);sudo docker rm postgres-latest-$(hostname);
+version="$1"
+[[ -z "$version" ]] && { echo "Error: Missing docker version tag e.g. latest, 12.5"; exit 1; }
 
-echo "Creating postgres-latest-$(hostname) docker container."
-sudo docker run --name postgres-latest-$(hostname) -p 5432:5432 -e POSTGRES_DB=RedisLabsCDC -e POSTGRES_USER=rediscdc -e POSTGRES_PASSWORD=Redis@123 -d postgres:latest
+# delete the existing postgres:$version container if it exist
+sudo docker kill postgres-$version-$(hostname);sudo docker rm postgres-$version-$(hostname);
+
+echo "Creating postgres-$version-$(hostname) docker container."
+sudo docker run --name postgres-$version-$(hostname) -e POSTGRES_DB=RedisLabsCDC -e POSTGRES_USER=rediscdc -e POSTGRES_PASSWORD=Redis@123 -p 5432:5432 -d postgres:$version -c wal_level=logical -c max_wal_senders=1 -c max_replication_slots=1
 
 sleep 30s
 
 echo "Creating RedisLabsCDC database and emp table."
 #run the setup script to create the DB and the table in the DB
-sudo docker cp postgres_cdc.sql postgres-latest-$(hostname):postgres_cdc.sql
-sudo docker exec -it postgres-latest-$(hostname) bash -c 'psql -U"$POSTGRES_USER" -d"$POSTGRES_DB" < postgres_cdc.sql'
+sudo docker cp postgres_cdc.sql postgres-$version-$(hostname):postgres_cdc.sql
+sudo docker exec -it postgres-$version-$(hostname) bash -c 'psql -U"$POSTGRES_USER" -d"$POSTGRES_DB" < postgres_cdc.sql'
 echo ""
