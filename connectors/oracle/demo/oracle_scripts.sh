3019e6cc0dae30c00bf4c3de27bbf70cfe3453ea
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2021-05-20 04:50:19 -0400

    updated

--- connectors/oracle/demo/oracle_scripts.sh
+++ connectors/oracle/demo/oracle_scripts.sh
@@ -6,5 +6,3 @@ version="$1"
 echo "Setting up LogMiner and loading sample HR schema.."
 sudo docker cp setup_logminer.sh oracle-$version-$(hostname):/tmp/setup_logminer.sh
 sudo docker exec -it oracle-$version-$(hostname) bash -c "/tmp/setup_logminer.sh"
-sudo docker cp setup_hr.sh oracle-$version-$(hostname):/tmp/setup_hr.sh
-sudo docker exec -it oracle-$version-$(hostname) bash -c "/tmp/setup_hr.sh"
