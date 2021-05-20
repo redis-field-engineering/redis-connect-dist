c83365d18ae3eb1c3611521c0f149ec8eb76e9a6
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2021-05-20 13:20:27 -0400

    migrated to setup

--- connectors/oracle/demo/oracle_scripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-version="$1"
-[[ -z "$version" ]] && { echo "Error: Missing docker version tag e.g. 12.2.0.1-ee, 19.3.0-ee"; exit 1; }
-
-echo "Setting up LogMiner and loading sample HR schema.."
-sudo docker cp setup_logminer.sh oracle-$version-$(hostname):/tmp/setup_logminer.sh
-sudo docker exec -it oracle-$version-$(hostname) bash -c "/tmp/setup_logminer.sh"
