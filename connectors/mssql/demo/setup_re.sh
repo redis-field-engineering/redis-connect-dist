8c91808158eff1b280e8b6e89e9c8bcd4f99f14d
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2021-05-24 10:16:59 -0400

    updated RTS

--- connectors/mssql/demo/setup_re.sh
+++ connectors/mssql/demo/setup_re.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo docker kill re-node1;sudo docker rm re-node1;
 sudo docker kill redisinsight;sudo docker rm redisinsight;
+sudo docker kill grafana; sudo docker rm grafana;
 # Uncomment this to pull the newer version of redislabs/redis docker image in case the latest tag has been upgraded
 sudo docker rmi -f $(sudo docker images | grep redislabs | awk '{print $3}')
 # Start 1 docker container sincce we can't do HA with vanilla docker instance. Use docker swarm, RE on VM's or RE's K8s operator to achieve HA, clustering etc.
@@ -18,15 +19,17 @@ sudo docker exec -it re-node1 bash -c "/opt/redislabs/bin/rladmin info cluster"
 echo "Creating databases..."
 rm create_demodb.sh
 tee -a create_demodb.sh <<EOF
-curl -v -k -L -u demo@redislabs.com:redislabs --location-trusted -H Content-type:application/json -d '{ "name": "RedisCDC-Target-db", "port": 12000, "memory_size": 1000000000, "type" : "redis", "replication": false, "module_list": [ {"module_args": "PARTITIONS AUTO", "module_id": "737bf664f5f59396f64db3afadd111ff", "module_name": "search", "semantic_version": "2.0.6"} ] }' https://localhost:9443/v1/bdbs
-curl -v -k -L -u demo@redislabs.com:redislabs --location-trusted -H "Content-type:application/json" -d '{"name": "RedisCDC-JobConfig-Metrics-db", "type":"redis", "replication": false, "memory_size":1000000000, "port":12001, "module_list": [{"module_args": "", "module_id": "23f3f39a1fdc76cf122f914afdf164cb", "module_name": "timeseries", "semantic_version": "1.4.7"}]}' https://localhost:9443/v1/bdbs
+curl -v -k -L -u demo@redislabs.com:redislabs --location-trusted -H "Content-type:application/json" -d '{ "name": "RedisCDC-Target-db", "port": 12000, "memory_size": 1000000000, "type" : "redis", "replication": false, "module_list": [ {"module_args": "PARTITIONS AUTO", "module_id": "737bf664f5f59396f64db3afadd111ff", "module_name": "search", "semantic_version": "2.0.6"} ] }' https://localhost:9443/v1/bdbs
+curl -v -k -L -u demo@redislabs.com:redislabs --location-trusted -H "Content-type:application/json" -d '{"name": "RedisCDC-JobConfig-Metrics-db", "type":"redis", "replication": false, "memory_size":1000000000, "port":12001, "module_list": [{"module_args": "", "module_id": "db0925a9d032ce983bbc83557b4f5a04", "module_name": "timeseries", "semantic_version": "1.4.8"}]}' https://localhost:9443/v1/bdbs
 EOF
+sleep 20
 sudo docker cp create_demodb.sh re-node1:/opt/create_demodb.sh
 sudo docker exec --user root -it re-node1 bash -c "chmod 777 /opt/create_demodb.sh"
 sudo docker exec -it re-node1 bash -c "/opt/create_demodb.sh"
 echo ""
 
 echo "Creating idx:emp index for search.."
+sleep 10
 sudo docker exec -it re-node1 bash -c "/opt/redislabs/bin/redis-cli -p 12000 ft.create idx:emp on hash prefix 1 'emp:' schema EmpNum numeric sortable FName text sortable LName text Job tag sortable Manager numeric HireDate text Salary numeric Commission numeric Department numeric"
 sudo docker exec -it re-node1 bash -c "/opt/redislabs/bin/redis-cli -p 12000 ft.info idx:emp"
 echo "Creating idx:cust index for search.."
@@ -42,6 +45,8 @@ echo "You can open a browser and access Redis Enterprise Admin UI at https://127
 echo "To connect using RedisInsight or redis-cli, please use the exposed port from the node where master shard for the database resides."
 echo "Creating RedisInsight in docker container.."
 sudo docker run -d --name redisinsight -p 18001:8001 -v redisinsight:/db redislabs/redisinsight:latest
+echo "Creating Grafana with redis-datasource in docker container.."
+sudo docker run -d -p 3000:3000 --name=grafana -e "GF_INSTALL_PLUGINS=redis-datasource" grafana/grafana
 echo "You can open a browser and access RedisInsight client UI at http://127.0.0.1:18001 (replace localhost with your ip/host) and add databases to monitor."
 echo "Please visit, https://docs.redislabs.com/latest/ri/using-redisinsight/add-instance/ for steps to add these databases to RedisInsight."
 echo "DISCLAIMER: This is best for local development or functional testing. Please see, https://docs.redislabs.com/latest/rs/getting-started/getting-started-docker"
