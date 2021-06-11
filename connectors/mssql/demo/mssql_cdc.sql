08520a5d6aac97e6788c684a3f6be226d9f0103a
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2021-06-11 11:16:05 -0400

    updated with RedisConnect

--- connectors/mssql/demo/mssql_cdc.sql
+++ connectors/mssql/demo/mssql_cdc.sql
@@ -4,24 +4,24 @@ GO
 IF NOT EXISTS (
        SELECT [name]
               FROM sys.databases
-              WHERE [name] = N'RedisLabsCDC'
+              WHERE [name] = N'RedisConnect'
 )
-CREATE DATABASE RedisLabsCDC
+CREATE DATABASE RedisConnect
 GO
 -- Check and enable database for CDC
 SELECT is_cdc_enabled
 FROM sys.databases
-WHERE name = 'RedisLabsCDC'
+WHERE name = 'RedisConnect'
 
 use RedisLabsCDC
 EXEC sys.sp_cdc_enable_db
 
 SELECT is_cdc_enabled
 FROM sys.databases
-WHERE name = 'RedisLabsCDC'
+WHERE name = 'RedisConnect'
 
--- Create emp table (Please note that MSSQL table names are case sensitive so this name must match the table name in RedisCDC JobConfig.yml)
-CREATE TABLE [RedisLabsCDC].[dbo].[emp] (
+-- Create emp table (Please note that MSSQL table names are case sensitive so this name must match the table name in RedisConnect JobConfig.yml)
+CREATE TABLE [RedisConnect].[dbo].[emp] (
     [empno] int NOT NULL,
     [fname] varchar(50),
     [lname] varchar(50),
@@ -42,5 +42,5 @@ EXEC sys.sp_cdc_enable_table @source_schema = 'dbo'
 
 -- Query and check the CDC setup
 -- Use this Stored proc to return the change data capture configuration for each table enabled for change data capture in the current database.
-USE RedisLabsCDC;
+USE RedisConnect;
 EXEC sys.sp_cdc_help_change_data_capture;
