95f958ed08794bbdcc482b974d26d74f1cb9d0a8
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2021-06-15 11:31:20 -0400

    updated db

--- connectors/mssql/demo/insert_mssql.sh
+++ connectors/mssql/demo/insert_mssql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
   
 sudo docker cp insert.sql mssql2017-$(hostname):insert.sql
-sudo docker exec -it mssql2017-$(hostname) /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "Redis@123" -d RedisLabsCDC -i insert.sql
+sudo docker exec -it mssql2017-$(hostname) /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "Redis@123" -d RedisConnect -i insert.sql
 echo ""
