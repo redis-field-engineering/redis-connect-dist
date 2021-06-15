44278073e2a43a65e93ef228e33c9c6f4b6e2d39
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2021-06-15 11:29:19 -0400

    updated db

--- connectors/mssql/demo/update_mssql.sh
+++ connectors/mssql/demo/update_mssql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
   
 sudo docker cp update.sql mssql2017-$(hostname):update.sql
-sudo docker exec -it mssql2017-$(hostname) /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "Redis@123" -d RedisLabsCDC -i update.sql
+sudo docker exec -it mssql2017-$(hostname) /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "Redis@123" -d RedisConnect -i update.sql
 echo ""
