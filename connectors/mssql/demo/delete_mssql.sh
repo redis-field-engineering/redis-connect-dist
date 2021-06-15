9b1673ecca69d50ef0c86a6a604b88f36250a3c7
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2021-06-15 11:31:37 -0400

    updated db

--- connectors/mssql/demo/delete_mssql.sh
+++ connectors/mssql/demo/delete_mssql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
   
 sudo docker cp delete.sql mssql2017-$(hostname):delete.sql
-sudo docker exec -it mssql2017-$(hostname) /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "Redis@123" -d RedisLabsCDC -i delete.sql
+sudo docker exec -it mssql2017-$(hostname) /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "Redis@123" -d RedisConnect -i delete.sql
 echo ""
