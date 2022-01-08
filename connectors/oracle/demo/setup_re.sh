c8695343b67621a80406132f9ee5727f50832f86
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2022-01-08 00:40:39 -0500

    Updated re scripts to load module info dynamically.

--- connectors/oracle/demo/setup_re.sh
+++ connectors/oracle/demo/setup_re.sh
@@ -47,7 +47,7 @@ sleep 20
 
 sudo docker cp create_demodb.sh re-node1:/opt/create_demodb.sh
 sudo docker exec --user root -it re-node1 bash -c "chmod 777 /opt/create_demodb.sh"
-//g" /opt/create_demodb.sh"t -it re-node1 bash -c "sed -i "s/
+sudo docker exec --user root -it re-node1 bash -c "sed -i "s/^M//g" /opt/create_demodb.sh"
 sudo docker exec -it re-node1 bash -c "/opt/create_demodb.sh"
 echo ""
 
