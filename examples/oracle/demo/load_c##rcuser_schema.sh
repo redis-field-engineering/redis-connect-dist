dfda9ea8d3d0e0a79854a66da268dabf81132d77
viragtripathi <virag.tripathi@gmail.com>
2023-07-03 13:07:22 -0400

    Added blob and clob tables in the example oracle setup

--- examples/oracle/demo/load_c##rcuser_schema.sh
+++ examples/oracle/demo/load_c##rcuser_schema.sh
@@ -21,6 +21,33 @@ sqlplus c##rcuser/rcpwd@ORCLPDB1 <<- EOF
   SELECT table_name, owner, LOGICAL_REPLICATION FROM all_tables WHERE owner='C##RCUSER' AND table_name='EMP'; 
   select count(*) from C##RCUSER.EMP;
 
+  create table C##RCUSER.CLOB_DATA1(
+  id          number(8) not null primary key,
+  clob_data1  clob
+  );
+
+ create table C##RCUSER.CLOB_DATA2(
+  id          number(8)	not null primary key,
+  clob_data2	clob
+  );
+
+ create table C##RCUSER.BLOB_DATA1(
+  id          number(8)	not null primary key,
+  blob_data1	blob
+  );
+
+  ALTER TABLE C##RCUSER.CLOB_DATA1 ADD SUPPLEMENTAL LOG DATA (ALL) COLUMNS;
+  SELECT table_name, owner, LOGICAL_REPLICATION FROM all_tables WHERE owner='C##RCUSER' AND table_name='CLOB_DATA1';
+  select count(*) from C##RCUSER.CLOB_DATA1;
+
+  ALTER TABLE C##RCUSER.CLOB_DATA2 ADD SUPPLEMENTAL LOG DATA (ALL) COLUMNS;
+  SELECT table_name, owner, LOGICAL_REPLICATION FROM all_tables WHERE owner='C##RCUSER' AND table_name='CLOB_DATA2';
+  select count(*) from C##RCUSER.CLOB_DATA2;
+
+  ALTER TABLE C##RCUSER.BLOB_DATA1 ADD SUPPLEMENTAL LOG DATA (ALL) COLUMNS;
+  SELECT table_name, owner, LOGICAL_REPLICATION FROM all_tables WHERE owner='C##RCUSER' AND table_name='BLOB_DATA1';
+  select count(*) from C##RCUSER.BLOB_DATA1;
+
   exit;
 EOF
 
